ee9407c2a96052ec61558c50ba3eee88fa3f2c46
谷藤圭介 <>
2025-01-13 14:16:21 +0900

    修正

--- providers/authProvider.ts
+++ providers/authProvider.ts
@@ -1,28 +1,39 @@
-// src/authProvider.ts
 import {
     signInWithEmailAndPassword,
     signOut,
 } from 'firebase/auth';
 import { AUTH_CHECK, AUTH_ERROR, AUTH_GET_PERMISSIONS, AUTH_LOGIN, AUTH_LOGOUT } from 'react-admin';
 import { auth } from '../firebaseConfig';
-const authProvider = async (type, params) => {
+
+type AuthProviderType = typeof AUTH_LOGIN | typeof AUTH_LOGOUT | typeof AUTH_ERROR | typeof AUTH_CHECK | typeof AUTH_GET_PERMISSIONS;
+
+interface AuthProviderParams {
+    username?: string;
+    password?: string;
+}
+
+const authProvider = async (type: AuthProviderType, params: AuthProviderParams) => {
     if (type === AUTH_LOGIN) {
         try {
             const { username, password } = params;
-            await signInWithEmailAndPassword(auth, username, password);
+            await signInWithEmailAndPassword(auth, username!, password!);
             const user = auth.currentUser;
             if (user) {
                 console.log(user);
-                localStorage.setItem("user", JSON.stringify(user));
-                // return JSON.stringify(user);
+                if (typeof window !== 'undefined') {
+                    window.localStorage.setItem("user", JSON.stringify(user));
+                }
                 return Promise.resolve();
             }
-        } catch {return Promise.reject();}
-        
+        } catch {
+            return Promise.reject();
+        }
     }
     if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('user');
-        localStorage.removeItem('role');
+        if (typeof window !== 'undefined') {
+            window.localStorage.removeItem('user');
+            window.localStorage.removeItem('role');
+        }
         await signOut(auth);
         return Promise.resolve();
     }
@@ -30,7 +41,7 @@ const authProvider = async (type, params) => {
         // ...
     }
     if (type === AUTH_CHECK) {
-        return localStorage.getItem('user') ? Promise.resolve() : Promise.reject();
+        return window.localStorage.getItem('user') ? Promise.resolve() : Promise.reject();
     }
     if (type === AUTH_GET_PERMISSIONS) {
         const role = localStorage.getItem('role');
