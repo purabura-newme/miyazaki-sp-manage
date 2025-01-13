6d0857569532b324df9dcc236c5ad32409ba2c73
谷藤圭介 <>
2025-01-13 17:57:25 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,15 @@ import { Amplify } from "aws-amplify";
 import dynamic from "next/dynamic";
 import outputs from "../src/amplifyconfiguration.json";
 
-const AdminAppComponent = dynamic(
-  () => import("../components/AdminApp"),
-  { ssr: false }
-)
+// AdminAppComponent を動的にインポートし、SSR を無効化
+const AdminAppComponent = dynamic(() => import("../components/AdminApp"), { ssr: false });
 
+// Amplify の設定
 Amplify.configure(outputs);
 
 const App = () => {
   return (
-    AdminAppComponent
+    <AdminAppComponent /> // JSX 要素としてレンダリング
   );
 };
 
